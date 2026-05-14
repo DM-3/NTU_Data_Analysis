3a4f2a22d1ed5610b62048b165a99050b26c609e
Daviscacha <david.spickenheuer@gmail.com>
2026-05-14 15:58:57 +0800

    tests

--- src/datasets.py
+++ src/datasets.py
@@ -24,3 +24,23 @@ class WM_811K():
             idx = indices[np.random.randint(0, len(indices))]
             ax.imshow(self.X[idx])
             ax.set_title(name)
+
+class WM_811K_preprocessed():
+    def __init__(self):
+        path = os.path.join('..', 'data', 'LSWMD_slimmed.pkl')
+        df = pd.read_pickle(path)
+        self.X = df['waferMap'].apply(np.array).to_numpy()
+        df['label_int'], lnames = pd.factorize(df['label'])
+        self.labels = df['label_int'].to_numpy()
+        self.label_names = lnames
+
+    def preview(self):
+        _, axs = plt.subplots(3,3)
+        axs = axs.flatten()
+        for i, (ax,name) in enumerate(zip(axs, self.label_names)):
+            idx = np.where(self.labels == i)[0][0]
+
+            indices = np.where(self.labels == i)[0]
+            idx = indices[np.random.randint(0, len(indices))]
+            ax.imshow(self.X[idx])
+            ax.set_title(name)
