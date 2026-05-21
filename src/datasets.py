1cf4e3d249d67ee3c5ff66bbdceebcc7e6f27bfb
DM-3 <m.werner.0@protonmail.com>
2026-05-21 17:27:04 +0800

    Create generator-based datasets for multi defect wafermaps

--- src/datasets.py
+++ src/datasets.py
@@ -6,50 +6,50 @@ import tensorflow as tf
 
 
 
-class WM_811K():
-    def __init__(self, imsize=(64,64)):
-        self.imsize = imsize
-        self._X = None
-        self._labels = None
-        self._label_names = None
+# class WM_811K():
+#     def __init__(self, imsize=(64,64)):
+#         self.imsize = imsize
+#         self._X = None
+#         self._labels = None
+#         self._label_names = None
 
-        path = os.path.join('..', 'data', 'LSWMD_slimmed.pkl')
-        self.df = pd.read_pickle(path)
+#         path = os.path.join('..', 'data', 'LSWMD_slimmed.pkl')
+#         self.df = pd.read_pickle(path)
 
-    @property
-    def X(self):
-        if self._X is None:
-            image_list = [tf.image.resize(
-                          tf.expand_dims(
-                          tf.convert_to_tensor(x), -1), self.imsize) 
-                          for x in self.df['waferMap']]
-            self._X = tf.convert_to_tensor(image_list)
-        return self._X
+#     @property
+#     def X(self):
+#         if self._X is None:
+#             image_list = [tf.image.resize(
+#                           tf.expand_dims(
+#                           tf.convert_to_tensor(x), -1), self.imsize) 
+#                           for x in self.df['waferMap']]
+#             self._X = tf.convert_to_tensor(image_list)
+#         return self._X
     
-    @property
-    def labels(self):
-        if self._labels is None:
-            lints, lnames = pd.factorize(self.df['label'])
-            self._labels = lints
-            self._label_names = lnames
-        return self._labels
+#     @property
+#     def labels(self):
+#         if self._labels is None:
+#             lints, lnames = pd.factorize(self.df['label'])
+#             self._labels = lints
+#             self._label_names = lnames
+#         return self._labels
     
-    @property
-    def label_names(self):
-        if self._label_names is None:
-            lints, lnames = pd.factorize(self.df['label'])
-            self._labels = lints
-            self._label_names = lnames
-        return self._label_names
+#     @property
+#     def label_names(self):
+#         if self._label_names is None:
+#             lints, lnames = pd.factorize(self.df['label'])
+#             self._labels = lints
+#             self._label_names = lnames
+#         return self._label_names
 
-    def preview(self):
-        _, axs = plt.subplots(3,3)
-        axs = axs.flatten()
-        for i, (ax,name) in enumerate(zip(axs, self.label_names)):
-            indices = np.where(self.labels == i)[0]
-            idx = indices[np.random.randint(0, len(indices))]
-            ax.imshow(self.X[idx])
-            ax.set_title(name)
+#     def preview(self):
+#         _, axs = plt.subplots(3,3)
+#         axs = axs.flatten()
+#         for i, (ax,name) in enumerate(zip(axs, self.label_names)):
+#             indices = np.where(self.labels == i)[0]
+#             idx = indices[np.random.randint(0, len(indices))]
+#             ax.imshow(self.X[idx])
+#             ax.set_title(name)
 
 
 
@@ -81,4 +81,3 @@ class WM_811K_preprocessed():
             
             ax.imshow(self.X[idx].squeeze())
             ax.set_title(name)
-
